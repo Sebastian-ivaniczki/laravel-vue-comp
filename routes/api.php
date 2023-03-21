1a0055a47b6f0f22dcc943ed1c5570ae93a1647c
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-03-21 12:13:31 +0100

    add route in api route

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/games', [GameController::class, 'index']);
+
+//route for details
+
+Route::get('/games/{game}', [GameController::class, 'show']);

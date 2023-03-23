fc7d12796b558414a3f9b24844fd9254f3c8b643
DarioGiusino <dario.giusino@gmail.com>
2023-03-23 08:43:28 +0100

    fix fillable game model

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -12,7 +12,7 @@ class Game extends Model
     use HasFactory;
 
     // fillable
-    protected $fillable = ['title', 'description', 'image', 'pusblished_year', 'vote', 'sell_price', 'banner_image', 'video_trailer', 'editor_id'];
+    protected $fillable = ['title', 'description', 'image', 'published_year', 'vote', 'sell_price', 'banner_image', 'video_trailer', 'editor_id'];
 
     protected $with = ['genres'];
 
